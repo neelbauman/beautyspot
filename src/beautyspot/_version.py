3f8fb750fdd7eddc071dd4f4bd53b2620eb120bb
Neel Bauman <neel.bauman@example.com>
2026-02-02 16:05:49 +0900

    v2.2.5

--- src/beautyspot/_version.py
+++ src/beautyspot/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '2.2.6.dev9+gbd456d23d.d20260202'
-__version_tuple__ = version_tuple = (2, 2, 6, 'dev9', 'gbd456d23d.d20260202')
+__version__ = version = '2.2.5'
+__version_tuple__ = version_tuple = (2, 2, 5)
 
 __commit_id__ = commit_id = None
