5c7f44411150592012cc709e7e56c52d35487202
Neel Bauman <neel.bauman@example.com>
2026-03-11 19:09:33 +0900

    save

--- test-esm.mjs
+++
@@ -1,2 +0,0 @@
-import * as gfmMod from 'https://esm.sh/turndown-plugin-gfm@1.0.2';
-console.log(Object.keys(gfmMod));
--- test_doorstop.py
+++
@@ -1,6 +0,0 @@
-import doorstop
-tree = doorstop.build()
-doc = tree.documents[0]
-item = list(doc)[0]
-print("UID:", item.uid)
-print("Doc Prefix:", item.document.prefix)
